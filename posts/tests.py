6e10a4ee600065b6227b73a8f2b97102d7931b62
Danil Lasuirya <d.r.lasuria@gmail.com>
2022-02-10 21:56:09 +0300

    test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from posts.models import Post
 
 # Create your tests here.
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text = 'just a simple test')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a simple test')
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text = 'it is another simple text' )
+
+    def test_view_url_exists(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('HomePage'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
